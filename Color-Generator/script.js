38c5268ef619c98605a736c93890cd385782f5ec
Ayan Sahil <ayans8557@gmail.com>
2025-11-20 13:27:41 +0530

    Add Color-Generator project to DOM repository

---
+++ Color-Generator/script.js
@@ -0,0 +1,11 @@
+let box = document.querySelector(".box");
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  let c1 = Math.floor(Math.random() * 256);
+  let c2 = Math.floor(Math.random() * 256);
+  let c3 = Math.floor(Math.random() * 256);
+
+ let colors= (`rgb(${c1},${c2},${c3})`);
+ box.style.backgroundColor = colors;
+});
