579b892814cb6429446f594b0ddf8cb78a77dc96
Ayan Sahil <ayans8557@gmail.com>
2025-11-20 14:11:08 +0530

    Dom2 Color-Generator

--- Color-Generator/script.js
+++ Color-Generator/script.js
@@ -6,6 +6,6 @@ btn.addEventListener("click", function () {
   let c2 = Math.floor(Math.random() * 256);
   let c3 = Math.floor(Math.random() * 256);
 
- let colors= (`rgb(${c1},${c2},${c3})`);
- box.style.backgroundColor = colors;
+  let colors= (`rgb(${c1},${c2},${c3})`);
+  box.style.backgroundColor = colors;
 });
