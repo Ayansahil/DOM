7d96435f591a8d17dcde290fe625f42cd4208698
Ayan Sahil <ayans8557@gmail.com>
2025-11-22 22:35:03 +0530

    Dom5 Instagram-like

--- Instagram/script.js
+++ Instagram/script.js
@@ -1,11 +1,31 @@
 let post = document.querySelector(".post-image img");
-let action = document.querySelector(".action-buttons");
-let heart = document.querySelector(".ri-heart-3-line");
+let heart = document.querySelector(".post-image i");
+let action = document.querySelector(".action-buttons i");
+
 
 post.addEventListener("dblclick", function () {
-  heart.classList.add("active");
-  heart.style.color='red';
-  console.log("click");
-  
+  heart.style.opacity = "1";
+  heart.style.transform = "translate(-50%, -50%) scale(1) rotate(-10deg)  ";
+  action.classList.add("ri-heart-3-fill");
+  action.classList.remove("ri-heart-3-line");
+  action.style.color = "red";
 
+  setTimeout(function () {
+    heart.style.opacity = "0";
+    heart.style.transform = "translate(-50%, -50%) scale(0)  rotate(-20deg) ";
+  }, 1000);
+});
+
+action.addEventListener("click", function () {
+  if (action.classList.contains("ri-heart-3-fill")) {
+    // If it's already liked, unlike it
+    action.classList.remove("ri-heart-3-fill");
+    action.classList.add("ri-heart-3-line");
+    action.style.color = "initial"; // Reverts to the default color
+  } else {
+    // If it's not liked, like it
+    action.classList.add("ri-heart-3-fill");
+    action.classList.remove("ri-heart-3-line");
+    action.style.color = "red";
+  }
 });
