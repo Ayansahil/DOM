e90095c9743588ae5d56ea980563f1f3b6501112
Ayan Sahil <ayans8557@gmail.com>
2025-11-22 15:58:20 +0530

    Dom4 Download-bar

--- Download-timeout/script.js
+++ Download-timeout/script.js
@@ -2,16 +2,29 @@ let btn = document.querySelector("button");
 let outer = document.querySelector(".outer");
 let inner = document.querySelector(".inner");
 let progress = document.querySelector(".progress");
+let h1 = document.querySelector("h1");
 
 let grow = 0;
+let tm = null;
 
 btn.addEventListener("click", () => {
-  let tm = setInterval(() => {
-    if (grow <= 99) {
-        grow++;
-      inner.style.width = `${grow}%`;
+  if (tm) return; // multiple click se bachne ke liye (optional)
+
+  tm = setInterval(() => {
+    if (grow < 100) {
+      grow++;
+      // 100% = 360deg, isliye 3.6 * grow
+      outer.style.background = `
+        radial-gradient(closest-side, #e3edf7 79%, transparent 80% 100%),
+        conic-gradient(#4caf50 ${grow * 3.6}deg, #e3edf7 0deg)
+      `;
       progress.innerHTML = `${grow}%`;
+      h1.innerHTML = "Downloading...";
+      h1.style.color = "black";
+    } else {
+      h1.innerHTML = "Downloaded!!!";
+      h1.style.color = "green";
+      clearInterval(tm);
     }
-  }, 200);
+  }, 50);
 });
-clearInterval(tm);
