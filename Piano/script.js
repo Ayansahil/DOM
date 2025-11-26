d5562bc42207483f563cf0ae8454afbc26b052f5
Ayan Sahil <ayans8557@gmail.com>
2025-11-26 20:46:37 +0530

    Piano

--- Piano/script.js
+++ Piano/script.js
@@ -1,36 +1,21 @@
-let allkeys = document.querySelectorAll(".key");
+let keys = document.querySelectorAll(".key");
 
-// Keyboard key -> Note mapping
-const keyMap = {
-  'a': 'C3', 's': 'D3', 'd': 'E3', 'f': 'F3', 'g': 'G3', 'h': 'A3', 'j': 'B3',
-  'z': 'C4', 'x': 'D4', 'c': 'E4', 'v': 'F4', 'b': 'G4', 'n': 'A4', 'm': 'B4'
-};
-
-// Note -> sound file mapping
-const noteToFile = {
-  'C3': 'c.wav', 'D3': 'd.wav', 'E3': 'e.wav', 'F3': 'f.wav', 'G3': 'g.wav',
-  'A3': 'a.wav', 'B3': 'b.wav', 'C4': 'c.wav', 'D4': 'd.wav', 'E4': 'e.wav',
-  'F4': 'f.wav', 'G4': 'g.wav', 'A4': 'a.wav', 'B4': 'b.wav'
-};
-
-// Mouse click: play sound based on data-note
-allkeys.forEach(function(key){
-  key.addEventListener("click", function() {
-    let note = this.getAttribute("data-note");
-    let file = noteToFile[note];
-    if (file) {
-      let sound = new Audio('/tunes/' + file);
-      sound.play();
-    }
+keys.forEach(function (key) {
+  key.addEventListener("click", function () {
+    let soundName = key.dataset.note; // data-note se naam milega
+    playSound(soundName);
+   
   });
 });
 
-// Keyboard (keydown): play sound based on pressed key
-document.addEventListener("keydown", function(e) {
-  let note = keyMap[e.key.toLowerCase()];
-  let file = noteToFile[note];
-  if (file) {
-    let sound = new Audio('/tunes/' + file);
-    sound.play();
-  }
+document.body.addEventListener("keydown", function (event) {
+  let keyPressed = event.key.toLowerCase();
+  playSound(keyPressed);
 });
+
+// 4) Yeh function sound play karega
+function playSound(name) {
+  let audio = new Audio(`./sounds/${name}.wav`);
+  audio.currentTime = 0;
+  audio.play();
+}
